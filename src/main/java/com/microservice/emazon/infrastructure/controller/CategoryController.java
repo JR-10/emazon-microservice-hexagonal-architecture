3828ad4ab79d0e596c7394a64c5eb20269a46018
Jonathan Rivera <arbeyjr@gmail.com>
2024-08-21 19:32:14 -0500

    implementar documentacion API con OpenApi v1

--- src/main/java/com/microservice/emazon/infrastructure/controller/CategoryController.java
+++ src/main/java/com/microservice/emazon/infrastructure/controller/CategoryController.java
@@ -3,6 +3,12 @@ package com.microservice.emazon.infrastructure.controller;
 import com.microservice.emazon.application.dto.CategoryRequestDto;
 import com.microservice.emazon.application.dto.CategoryResponseDto;
 import com.microservice.emazon.application.handler.ICategoryHandler;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.ArraySchema;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Pageable;
 import org.springframework.http.HttpStatus;
@@ -19,17 +25,36 @@ public class CategoryController {
     private final ICategoryHandler categoryHandler;
 
 
+    @Operation(summary = "Get all the categories")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "All categories returned",
+                    content = @Content(mediaType = "application/json", array = @ArraySchema(schema = @Schema(implementation = CategoryResponseDto.class)))),
+            @ApiResponse(responseCode = "404", description = "No data found", content = @Content)
+    })
     @GetMapping("/getAllCategories")
     public ResponseEntity<List<CategoryResponseDto>> getAllCategories(){
         return ResponseEntity.ok().body(categoryHandler.getAllCategories());
     }
 
+
+    @Operation(summary = "Add a new category")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "201", description = "Category created", content = @Content),
+            @ApiResponse(responseCode = "409", description = "Category already exists", content = @Content)
+    })
     @PostMapping("/addCategory")
     public ResponseEntity<String> createCategory(@RequestBody CategoryRequestDto categoryRequestDto) {
         categoryHandler.saveCategory(categoryRequestDto);
         return  ResponseEntity.status(HttpStatus.CREATED).body("Ha creado exitosamente la categoría " +categoryRequestDto.getName());
     }
 
+
+    @Operation(summary = "Get a category by ordering")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Category found",
+                    content = @Content(mediaType = "application/json", schema = @Schema(implementation = CategoryResponseDto.class))),
+            @ApiResponse(responseCode = "404", description = "Category not found", content = @Content)
+    })
     @GetMapping("/getCategories/{ordering}")
     public ResponseEntity<List<CategoryResponseDto>> getCategories(@PathVariable String ordering, Pageable pageable){
         return ResponseEntity.ok().body(categoryHandler.getCategories(ordering,pageable).getContent());
